3756a97139e631e3f7a5259609214b1bb42ac32b
jbialon32 <jbialon32@gmail.com>
2021-04-28 12:04:28 -0500

    Added Driver class and a User data model

---
+++ intro-2-java/src/com/revature/intro/Driver.java
@@ -0,0 +1,11 @@
+package com.revature.intro;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jbialon
+ * Date: 4/28/2021
+ * Time: 11:36 AM
+ * Description: {Insert Description}
+ */
+public class Driver {
+}
---
+++ intro-2-java/src/com/revature/intro/models/AppUser.java
@@ -0,0 +1,11 @@
+package com.revature.intro.models;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jbialon
+ * Date: 4/28/2021
+ * Time: 11:46 AM
+ * Description: {Insert Description}
+ */
+public class AppUser {
+}
