5d90054cd2ba63b62af2b56109381c5a7a25a504
mtuck317 <31971432+mtuck317@users.noreply.github.com>
2021-04-28 11:47:53 -0500

    Added app user model

--- Intro To Java/src/com/revature/intro/Driver.java
+++ Intro To Java/src/com/revature/intro/Driver.java
@@ -1,7 +1,9 @@
 package com.revature.intro;
 
 public class Driver {
+
     public static void main(String[] args){
         System.out.println("hello world");
+
     }
 }
---
+++ Intro To Java/src/com/revature/intro/models/AppUser.java
@@ -0,0 +1,4 @@
+package com.revature.intro.models;
+
+public class AppUser {
+}
