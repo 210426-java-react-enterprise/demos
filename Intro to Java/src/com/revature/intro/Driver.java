59a11c1c1257783e8412e300d88fcb69333f6727
Austin <austinknauer@alumni.colostate.edu>
2021-04-28 11:31:10 -0600

    Created Intro Package, Driver Class, Model Package, and AppUser Package

---
+++ Intro to Java/src/com/revature/intro/Driver.java
@@ -0,0 +1,11 @@
+package com.revature.intro;
+
+import com.revature.intro.models.AppUser;
+
+public class Driver {
+
+    public static void main (String[] args) {
+        AppUser newUser = new AppUser("austinknauer", "austin.knauer@revature.net", "1234", "Austin", "knauer", 24);
+    }
+
+}
---
+++ Intro to Java/src/com/revature/intro/models/AppUser.java
@@ -0,0 +1,72 @@
+package com.revature.intro.models;
+
+/*
+POJO: Plain Ol' Java OBject, only has setters and getters
+ */
+
+public class AppUser {
+
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public AppUser(String username, String email, String password, String firstName, String lastName, int age) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
