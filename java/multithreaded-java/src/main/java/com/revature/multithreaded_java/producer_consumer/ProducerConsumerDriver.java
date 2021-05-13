1ba3982c5987652e2a7009faccf9f146e5f1db50
deterge <45431127+Deterge@users.noreply.github.com>
2021-05-13 18:09:49 -0400

    changes made

--- java/multithreaded-java/src/main/java/com/revature/multithreaded_java/producer_consumer/ProducerConsumerDriver.java
+++ java/multithreaded-java/src/main/java/com/revature/multithreaded_java/producer_consumer/ProducerConsumerDriver.java
@@ -1,4 +1,29 @@
 package com.revature.multithreaded_java.producer_consumer;
 
 public class ProducerConsumerDriver {
+
+    public static void main(String[] args) {
+
+        final Object monitor = new Object();
+        CustomBuffer buffer = new CustomBuffer();
+
+        Thread producerThread = new Thread(producerTask);
+        producerThread.setName("ProducerThread");
+
+        Thread consumerThread = new Thread(consumerTask);
+        consumerThread.setName("ConsumerThread");
+
+        producerThread.setPriority(3);
+        consumerThread.setPriority(10);
+
+        producerThread.start();
+        consumerThread.start();
+
+        try {
+            producerThread.join();
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
