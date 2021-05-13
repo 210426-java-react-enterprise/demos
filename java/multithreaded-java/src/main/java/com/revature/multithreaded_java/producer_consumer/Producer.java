67d5cda3f447594668a366996baf29c4aa16f0ea
Wezley <wezley.singleton@gmail.com>
2021-05-13 17:46:31 -0400

    pushing for Sheckeem

---
+++ java/multithreaded-java/src/main/java/com/revature/multithreaded_java/producer_consumer/Producer.java
@@ -0,0 +1,35 @@
+package com.revature.multithreaded_java.producer_consumer;
+
+public class Producer {
+
+    private final Object monitor;
+    private CustomBuffer buffer;
+
+    public Producer(CustomBuffer buffer, Object monitor) {
+        this.monitor = monitor;
+        this.buffer = buffer;
+    }
+
+    public void produce() {
+
+        synchronized (monitor) {
+
+            if (buffer.isFull()) {
+                System.out.println("Buffer full, pausing producer thread.");
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            buffer.getBufferArray()[buffer.getCount()] = 1;
+            buffer.incrementCount();
+
+            System.out.println("Produced new value! Notifying monitor.");
+            monitor.notify();
+
+        }
+
+    }
+}
---
+++ java/multithreaded-java/src/main/java/com/revature/multithreaded_java/producer_consumer/ProducerConsumerDriver.java
@@ -0,0 +1,11 @@
+package com.revature.multithreaded_java.producer_consumer;
+
+public class ProducerConsumerDriver {
+
+    public static void main(String[] args) {
+
+        final Object m
+
+    }
+
+}
