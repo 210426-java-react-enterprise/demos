0af698b2e68f95477afe4388c82873090c176702
diuguide <everett.diuguid@gmail.com>
2021-06-21 15:21:50 -0400

    redux demo with comments

--- ui/redux/src/components/Buttons/index.tsx
+++ ui/redux/src/components/Buttons/index.tsx
@@ -1,5 +1,21 @@
 import { Row, Col, Button } from "react-bootstrap";
-import {  useDispatch } from "react-redux";
+/*
+  Import redux hooks into the component
+  - useSelector() - this will give you access to the state declared inside of your slice, you will pass in a reference to the slice
+  - useDispatch() - this will be your dispatcher, initialize in component with const dispatch = useDispatch();
+  - dispatch actions by useing dispatch(actionYouWantToDispatch());
+  - if you have a payload you want to send back to store to be stored in state and if your action takes a payload 
+    (as defined in the state slice) pass the payload into the action
+  - dispatch(action(payload))
+  - if the payload is an object, dispatch({ key: value, key2: value2 })
+*/
+import { useDispatch } from "react-redux";
+
+/*
+  - Import the actions and state object that is exported from state slice to be used in component
+  - These are imported from the state slice, they represent individual reducers
+  - Reducers are defined in the state slice, they manipulate your state
+*/
 import {
   increment,
   decrement,
@@ -10,51 +26,49 @@ import {
 } from "../../features/Counter/counterSlice";
 
 const ButtonGroup = () => {
+  //Initilize your dispatcher
+  const dispatch = useDispatch();
 
-      const dispatch = useDispatch();
-      
-    const handleIncrement = (e: any) => {
-        e.preventDefault();
-        dispatch(isLoading());
-        setTimeout(() => {
-          dispatch(increment());
-          dispatch(isLoaded());
-        }, 1000);
-      };
+  const handleIncrement = (e: any) => {
+    e.preventDefault();
+    // dispatch an action
+    dispatch(isLoading());
+    setTimeout(() => {
+      // you may also dispatch several actions at once, one after the other, giving you more control over the application state.
+      dispatch(increment());
+      dispatch(isLoaded());
+    }, 1000);
+  };
 
-    const handleDecrement = (e: any) => {
-        e.preventDefault();
-        dispatch(isLoading());
-        setTimeout(() => {
-          dispatch(decrement());
-          dispatch(isLoaded());
-        }, 1000);
-    };
-    
-    const handleReset = (e: any) => {
-      e.preventDefault();
-      dispatch(isLoading());
-      setTimeout(() => {
-        dispatch(resetCount());
-        dispatch(isLoaded());
-      }, 1000);
-    };
+  const handleDecrement = (e: any) => {
+    e.preventDefault();
+    dispatch(isLoading());
+    setTimeout(() => {
+      dispatch(decrement());
+      dispatch(isLoaded());
+    }, 1000);
+  };
 
+  const handleReset = (e: any) => {
+    e.preventDefault();
+    dispatch(isLoading());
+    setTimeout(() => {
+      dispatch(resetCount());
+      dispatch(isLoaded());
+    }, 1000);
+  };
 
+  return (
+    <Row>
+      <Col className="d-flex justify-content-center">
+        <Button onClick={handleIncrement}>Add</Button>
 
-    return (
-      <Row>
-            <Col className="d-flex justify-content-center">
-            <Button onClick={handleIncrement}>Add</Button>
-        
-          <Button onClick={handleDecrement}>Subtract</Button>
-        
-          <Button onClick={handleReset}>Reset</Button>
-            </Col>
-          
-       
-      </Row>
-    );
+        <Button onClick={handleDecrement}>Subtract</Button>
+
+        <Button onClick={handleReset}>Reset</Button>
+      </Col>
+    </Row>
+  );
 }
 
 export default ButtonGroup;
--- ui/redux/src/index.tsx
+++ ui/redux/src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Import provider from react-redux
 import { Provider } from "react-redux";
+
+// import store from where ever you have your store.ts file defined
 import { store } from "./app/store";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
+Wrap entire application with Provider tags and give it the attribute store={store} to provide the entire application with the redux store
+*/
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode>    
     <Provider store={store}>
       <App />
     </Provider>
